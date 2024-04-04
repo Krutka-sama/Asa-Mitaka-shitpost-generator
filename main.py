9a6c4342bdcf80ce5cbc177dd07ff493dd683bf8
Krutka-sama <epervyshev@mail.ru>
2024-04-05 00:18:20 +0300

    regex triggers

--- main.py
+++ main.py
@@ -1,53 +1,57 @@
 import asyncio
 import logging
 import sys
+import re
+import random
 from decouple import config
-
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.utils.markdown import hbold
-
+from aiogram import Bot, Dispatcher, Router, types, F
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = config('BOT_TOKEN')
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
+triggers = {
+    'ТРАХ': r"\b\w*трах\w*\b",
+    'ГОВНО': r"\b\w+(?:core|кор)\b",
+    'Планета ТРАХА': r"(?:планет[аеыуо]?\s?техно|пт)\b"
 
-@dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+}
+# db={}
+# c=5
+# c1=10
 
-
-@dp.message()
+@dp.message(F.text)
 async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
+    text = message.text.lower()
+    for response, pattern in triggers.items():
+        if re.search(pattern, text):
+            await message.answer(text=response)
+            break
+    # rng = random.SystemRandom()
+    # num = rng.randrange(8, 20)
+    # print(db)
+    # print(num)
+    # if num > c1 and str(message.chat.id) in db:
+    #     await message.answer_photo(photo=db[str(message.chat.id)])
+        # else:
+        #     await message.send_copy(chat_id=message.chat.id)
+        #     break
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
+# @dp.message(F.photo)
+# async def echo_photo(message: types.Message) -> None:
+#     rng = random.SystemRandom()
+#     num = rng.randrange(1,10)
+#     print(num)
+#     if num <= c:
+#         db[str(message.chat.id)]=message.photo[-1].file_id
 
+@dp.message(F.sticker)
+async def echo_sticker(message: types.Message) -> None:
+    await message.send_copy(chat_id=message.chat.id)
 
 async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
+    bot = Bot(TOKEN)
     await dp.start_polling(bot)
 
 
