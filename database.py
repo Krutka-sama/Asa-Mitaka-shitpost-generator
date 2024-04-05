9d1d2b3ef6dac9e3b93c35f3c6662e61ac55725e
Krutka-sama <epervyshev@mail.ru>
2024-04-05 14:33:46 +0300

    basic db

---
+++ database.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+async def connect(db_name):
+    global db, cursor
+    db = sqlite3.connect(db_name)
+    cursor = db.cursor()
+
+async def close(db):
+    db.close()
+
+async def create_table():
+    cursor.execute('''CREATE TABLE IF NOT EXISTS chat (
+                            id INTEGER PRIMARY KEY,
+                            chat_id INTEGER UNIQUE,
+                            text TEXT,
+                            image TEXT,
+                            count INTEGER DEFAULT 0
+                        )''')
+    cursor.connection.commit()
+
+async def check_chat_id(chat_id):
+    cursor.execute("SELECT * FROM chat WHERE chat_id = ?", (chat_id,))
+    existing_chat = cursor.fetchone()
+    if not existing_chat:
+        cursor.execute("INSERT INTO chat (chat_id) VALUES (?)", (chat_id,))
+        cursor.connection.commit()
+
+async def insert_text(chat_id, text):
+    cursor.execute("UPDATE chat SET text = ? WHERE chat_id = ?",
+                         (text, chat_id))
+    cursor.connection.commit()
+
+async def insert_image(chat_id, image):
+    cursor.execute("UPDATE chat SET image = ? WHERE chat_id = ?",
+                         (image, chat_id))
+    cursor.connection.commit()
+
+async def increment_count(chat_id):
+    await check_chat_id(chat_id)
+    cursor.execute("UPDATE chat SET count = count + 1 WHERE chat_id = ?",
+                         (chat_id,))
+    cursor.connection.commit()
+
+async def reset_count(chat_id):
+    await check_chat_id(chat_id)
+    cursor.execute("UPDATE chat SET count = 0 WHERE chat_id = ?",
+                         (chat_id,))
+    cursor.connection.commit()
+
+async def get_text_message(chat_id):
+    cursor.execute("SELECT text FROM chat WHERE chat_id = ?", (chat_id,))
+    row = cursor.fetchone()
+    if row:
+        return row[0]
+    else:
+        return None
+
+async def get_image_message(chat_id):
+    cursor.execute("SELECT image FROM chat WHERE chat_id = ?", (chat_id,))
+    row = cursor.fetchone()
+    if row:
+        return row[0]
+    else:
+        return None
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import re
 import random
 from decouple import config
 from aiogram import Bot, Dispatcher, Router, types, F
-# Bot token can be obtained via https://t.me/BotFather
-TOKEN = config('BOT_TOKEN')
+from database import connect, create_table, insert_text, insert_image, increment_count
 
-# All handlers should be attached to the Router (or Dispatcher)
+TOKEN = config('BOT_TOKEN')
+NAME = config('DB_NAME')
 dp = Dispatcher()
 
 triggers = {
@@ -17,41 +17,40 @@ triggers = {
     'Планета ТРАХА': r"(?:планет[аеыуо]?\s?техно|пт)\b"
 
 }
-# db={}
-# c=5
-# c1=10
+
 
 @dp.message(F.text)
 async def echo_handler(message: types.Message) -> None:
     text = message.text.lower()
+    await increment_count(message.chat.id)
+    await insert_text(message.chat.id, text)
     for response, pattern in triggers.items():
         if re.search(pattern, text):
             await message.answer(text=response)
             break
-    # rng = random.SystemRandom()
-    # num = rng.randrange(8, 20)
-    # print(db)
-    # print(num)
-    # if num > c1 and str(message.chat.id) in db:
-    #     await message.answer_photo(photo=db[str(message.chat.id)])
-        # else:
-        #     await message.send_copy(chat_id=message.chat.id)
-        #     break
 
-# @dp.message(F.photo)
-# async def echo_photo(message: types.Message) -> None:
-#     rng = random.SystemRandom()
-#     num = rng.randrange(1,10)
-#     print(num)
-#     if num <= c:
-#         db[str(message.chat.id)]=message.photo[-1].file_id
+
+@dp.message(F.photo)
+async def echo_photo(message: types.Message) -> None:
+    await increment_count(message.chat.id)
+    await insert_image(message.chat.id, message.photo[-1].file_id)
+
 
 @dp.message(F.sticker)
 async def echo_sticker(message: types.Message) -> None:
+    await increment_count(message.chat.id)
     await message.send_copy(chat_id=message.chat.id)
+
+
+@dp.message(~F.text & ~F.photo & ~F.sticker)
+async def echo_any(message: types.Message):
+    await increment_count(message.chat.id)
+
 
 async def main() -> None:
     bot = Bot(TOKEN)
+    await connect(NAME)
+    await create_table()
     await dp.start_polling(bot)
 
 
