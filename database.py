b249a78db55d5b5df82e0aecc534815563648ea0
Krutka-sama <epervyshev@mail.ru>
2024-04-06 03:01:27 +0300

    beta 0.1

--- database.py
+++ database.py
@@ -1,61 +1,77 @@
 import sqlite3
+
+MAX = 50
+
+
 async def connect(db_name):
     global db, cursor
     db = sqlite3.connect(db_name)
     cursor = db.cursor()
 
+
 async def close():
     db.close()
 
+
 async def create_table():
-    cursor.execute('''CREATE TABLE IF NOT EXISTS chat (
+    cursor.execute('''CREATE TABLE IF NOT EXISTS message (
+                               id INTEGER PRIMARY KEY,
+                               chat_id INTEGER,
+                               text TEXT
+                           )''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS image (
+                               id INTEGER PRIMARY KEY,
+                               chat_id INTEGER,
+                               image TEXT
+                           )''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS ban (
                             id INTEGER PRIMARY KEY,
-                            chat_id INTEGER UNIQUE,
-                            text TEXT,
-                            image TEXT,
-                            count INTEGER DEFAULT 0
+                            chat_id INTEGER UNIQUE
                         )''')
     cursor.connection.commit()
 
-async def check_chat_id(chat_id):
-    cursor.execute("SELECT * FROM chat WHERE chat_id = ?", (chat_id,))
-    existing_chat = cursor.fetchone()
-    if not existing_chat:
-        cursor.execute("INSERT INTO chat (chat_id) VALUES (?)", (chat_id,))
+
+async def insert_message(chat_id, text, max_rows=MAX):
+    cursor.execute("INSERT INTO message (chat_id, text) VALUES (?, ?)", (chat_id, text))
+    cursor.connection.commit()
+
+    cursor.execute("SELECT COUNT(*) FROM message WHERE chat_id = ?", (chat_id,))
+    num_chat_rows = cursor.fetchone()[0]
+
+    if num_chat_rows > max_rows:
+        cursor.execute(
+            f"DELETE FROM message WHERE id IN (SELECT id FROM message WHERE chat_id = ? ORDER BY id ASC LIMIT {num_chat_rows - max_rows})",
+            (chat_id,))
         cursor.connection.commit()
 
-async def insert_text(chat_id, text):
-    cursor.execute("UPDATE chat SET text = ? WHERE chat_id = ?",
-                         (text, chat_id))
+
+async def insert_image(chat_id, image, max_rows=MAX):
+    cursor.execute("INSERT INTO image (chat_id, image) VALUES (?, ?)", (chat_id, image))
     cursor.connection.commit()
 
-async def insert_image(chat_id, image):
-    cursor.execute("UPDATE chat SET image = ? WHERE chat_id = ?",
-                         (image, chat_id))
-    cursor.connection.commit()
+    cursor.execute("SELECT COUNT(*) FROM image WHERE chat_id = ?", (chat_id,))
+    num_image_rows = cursor.fetchone()[0]
 
-async def increment_count(chat_id):
-    await check_chat_id(chat_id)
-    cursor.execute("UPDATE chat SET count = count + 1 WHERE chat_id = ?",
-                         (chat_id,))
-    cursor.connection.commit()
+    if num_image_rows > max_rows:
+        cursor.execute(
+            f"DELETE FROM image WHERE id IN (SELECT id FROM image WHERE chat_id = ? ORDER BY id ASC LIMIT {num_image_rows - max_rows})",
+            (chat_id,))
+        cursor.connection.commit()
 
-async def reset_count(chat_id):
-    await check_chat_id(chat_id)
-    cursor.execute("UPDATE chat SET count = 0 WHERE chat_id = ?",
-                         (chat_id,))
-    cursor.connection.commit()
 
-async def get_text(chat_id):
-    cursor.execute("SELECT text FROM chat WHERE chat_id = ?", (chat_id,))
+async def get_random_text(chat_id):
+    cursor.execute("SELECT text FROM message WHERE chat_id = ? ORDER BY RANDOM() LIMIT 1", (chat_id,))
     row = cursor.fetchone()
     if row:
         return row[0]
     else:
         return None
 
-async def get_image(chat_id):
-    cursor.execute("SELECT image FROM chat WHERE chat_id = ?", (chat_id,))
+
+async def get_random_image(chat_id):
+    cursor.execute("SELECT image FROM image WHERE chat_id = ? ORDER BY RANDOM() LIMIT 1", (chat_id,))
     row = cursor.fetchone()
     if row:
         return row[0]
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import FSInputFile, BufferedInputFile
 from decouple import config
 from aiogram import Bot, Dispatcher, types, F
-from database import connect, close, create_table, insert_text, insert_image, increment_count, get_image, get_text
+from database import *
 from shitpost import shitpost
 
 TOKEN = config('BOT_TOKEN')
@@ -21,52 +21,58 @@ triggers = {
     'Планета ТРАХА': r"(?:планет[аеыуо]?\s?техно|пт)\b"
 
 }
+chance = 0.1
+async def post(message: types.Message, chance : float):
+    if random.random() <= chance:
+        file_id = await get_random_image(message.chat.id)
+        text = await get_random_text(message.chat.id)
+        if not file_id or not text:
+            await message.answer(text="dumbass")
+            return
+        file_info = await bot.get_file(file_id)
+
+        # path = "img/" + str(message.chat.id) + ".png"
+        # await bot.download_file(file_info.file_path, path)
+        # await shitpost(text, path)
+        # photo = FSInputFile(path)
+        # await message.answer_photo(photo)
+
+        #I have no idea how to use streams in python so this is probably trash garbage shit curse death
+        image_data = await bot.download_file(file_info.file_path)
+        modified_image_buffer = await shitpost(text, image_data)
+        img=BufferedInputFile(modified_image_buffer.getvalue(), "shitpost")
+        await message.answer_photo(img)
+        modified_image_buffer.truncate(0)
 @dp.message(CommandStart())
 async def start(message: types.Message):
-    file_id = await get_image(message.chat.id)
-    text = await get_text(message.chat.id)
-    file_info = await bot.get_file(file_id)
-
-    # path = "img/" + str(message.chat.id) + ".png"
-    # await bot.download_file(file_info.file_path, path)
-    # await shitpost(text, path)
-    # photo = FSInputFile(path)
-    # await message.answer_photo(photo)
-
-    #I have no idea how to use streams in python so this is probably trash garbage shit curse death
-    image_data = await bot.download_file(file_info.file_path)
-    modified_image_buffer = await shitpost(text, image_data)
-    img=BufferedInputFile(modified_image_buffer.getvalue(), "shitpost")
-    await message.answer_photo(img)
-    modified_image_buffer.truncate(0)
+    await post(message, 1)
 
 
 @dp.message(F.text)
 async def echo_handler(message: types.Message) -> None:
     text = message.text.lower()
-    await increment_count(message.chat.id)
-    await insert_text(message.chat.id, text)
+    await insert_message(message.chat.id, text)
     for response, pattern in triggers.items():
         if re.search(pattern, text):
             await message.answer(text=response)
             break
-
+    await post(message, chance)
 
 @dp.message(F.photo)
 async def echo_photo(message: types.Message) -> None:
-    await increment_count(message.chat.id)
     await insert_image(message.chat.id, message.photo[-1].file_id)
+    await post(message, chance)
 
 
 @dp.message(F.sticker)
 async def echo_sticker(message: types.Message) -> None:
-    await increment_count(message.chat.id)
     await message.send_copy(chat_id=message.chat.id)
+    await post(message, chance)
 
 
 @dp.message(~F.text & ~F.photo & ~F.sticker)
 async def echo_any(message: types.Message):
-    await increment_count(message.chat.id)
+    await post(message, chance)
 
 
 async def main() -> None:
