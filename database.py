f47746499d22307358e24fb178271e194e84858b
Krutka-sama <epervyshev@mail.ru>
2024-04-08 02:15:01 +0300

    Delete commands, unique messages and images in database

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 from decouple import config
+
 M_SCOPE = int(config('MESSAGES_SCOPE'))
 I_SCOPE = int(config('IMAGES_SCOPE'))
 
@@ -18,52 +19,52 @@ async def create_table():
     cursor.execute('''CREATE TABLE IF NOT EXISTS message (
                                id INTEGER PRIMARY KEY,
                                chat_id INTEGER,
-                               text TEXT
+                               message TEXT UNIQUE
                            )''')
 
     cursor.execute('''CREATE TABLE IF NOT EXISTS image (
                                id INTEGER PRIMARY KEY,
                                chat_id INTEGER,
-                               image TEXT
+                               image TEXT UNIQUE
                            )''')
 
-    # cursor.execute('''CREATE TABLE IF NOT EXISTS ban (
-    #                         id INTEGER PRIMARY KEY,
-    #                         chat_id INTEGER UNIQUE
-    #                     )''')
-    # cursor.connection.commit()
 
-
-async def insert_message(chat_id, text, max_rows=M_SCOPE):
-    cursor.execute("INSERT INTO message (chat_id, text) VALUES (?, ?)", (chat_id, text))
-    cursor.connection.commit()
-
-    cursor.execute("SELECT COUNT(*) FROM message WHERE chat_id = ?", (chat_id,))
-    num_chat_rows = cursor.fetchone()[0]
-
-    if num_chat_rows > max_rows:
-        cursor.execute(
-            f"DELETE FROM message WHERE id IN (SELECT id FROM message WHERE chat_id = ? ORDER BY id ASC LIMIT {num_chat_rows - max_rows})",
-            (chat_id,))
+async def insert_message(chat_id, message, max_rows=M_SCOPE):
+    cursor.execute("SELECT COUNT(*) FROM message WHERE chat_id = ? AND message = ?", (chat_id, message))
+    num_rows = cursor.fetchone()[0]
+    if not num_rows:
+        cursor.execute("INSERT INTO message (chat_id, message) VALUES (?, ?)", (chat_id, message))
         cursor.connection.commit()
+
+        cursor.execute("SELECT COUNT(*) FROM message WHERE chat_id = ?", (chat_id,))
+        num_chat_rows = cursor.fetchone()[0]
+
+        if num_chat_rows > max_rows:
+            cursor.execute(
+                f"DELETE FROM message WHERE id IN (SELECT id FROM message WHERE chat_id = ? ORDER BY id ASC LIMIT {num_chat_rows - max_rows})",
+                (chat_id,))
+            cursor.connection.commit()
 
 
 async def insert_image(chat_id, image, max_rows=I_SCOPE):
-    cursor.execute("INSERT INTO image (chat_id, image) VALUES (?, ?)", (chat_id, image))
-    cursor.connection.commit()
-
-    cursor.execute("SELECT COUNT(*) FROM image WHERE chat_id = ?", (chat_id,))
-    num_image_rows = cursor.fetchone()[0]
-
-    if num_image_rows > max_rows:
-        cursor.execute(
-            f"DELETE FROM image WHERE id IN (SELECT id FROM image WHERE chat_id = ? ORDER BY id ASC LIMIT {num_image_rows - max_rows})",
-            (chat_id,))
+    cursor.execute("SELECT COUNT(*) FROM image WHERE chat_id = ? AND image = ?", (chat_id, image))
+    num_rows = cursor.fetchone()[0]
+    if not num_rows:
+        cursor.execute("INSERT INTO image (chat_id, image) VALUES (?, ?)", (chat_id, image))
         cursor.connection.commit()
+
+        cursor.execute("SELECT COUNT(*) FROM image WHERE chat_id = ?", (chat_id,))
+        num_image_rows = cursor.fetchone()[0]
+
+        if num_image_rows > max_rows:
+            cursor.execute(
+                f"DELETE FROM image WHERE id IN (SELECT id FROM image WHERE chat_id = ? ORDER BY id ASC LIMIT {num_image_rows - max_rows})",
+                (chat_id,))
+            cursor.connection.commit()
 
 
 async def get_random_text(chat_id):
-    cursor.execute("SELECT text FROM message WHERE chat_id = ? ORDER BY RANDOM() LIMIT 1", (chat_id,))
+    cursor.execute("SELECT message FROM message WHERE chat_id = ? ORDER BY RANDOM() LIMIT 1", (chat_id,))
     row = cursor.fetchone()
     if row:
         return row[0]
@@ -78,3 +79,35 @@ async def get_random_image(chat_id):
         return row[0]
     else:
         return None
+
+
+async def delete_message(chat_id, message):
+    cursor.execute("SELECT COUNT(*) FROM message WHERE chat_id = ? AND message = ?", (chat_id, message))
+    num_rows = cursor.fetchone()[0]
+    if num_rows > 0:
+        cursor.execute("DELETE FROM message WHERE chat_id = ? AND message = ?", (chat_id, message))
+        cursor.connection.commit()
+        return True
+    else:
+        return False
+
+
+async def delete_image(chat_id, image):
+    cursor.execute("SELECT COUNT(*) FROM image WHERE chat_id = ? AND image = ?", (chat_id, image))
+    num_rows = cursor.fetchone()[0]
+    if num_rows > 0:
+        cursor.execute("DELETE FROM image WHERE chat_id = ? AND image = ?", (chat_id, image))
+        cursor.connection.commit()
+        return True
+    else:
+        return False
+
+
+async def delete_all_messages(chat_id):
+    cursor.execute("DELETE FROM message WHERE chat_id = ?", (chat_id,))
+    cursor.connection.commit()
+
+
+async def delete_all_images(chat_id):
+    cursor.execute("DELETE FROM image WHERE chat_id = ?", (chat_id,))
+    cursor.connection.commit()
--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import sys
 import re
 from random import random, choice
 from aiogram.filters import CommandStart, Command
-from aiogram.types import BufferedInputFile, FSInputFile
+from aiogram.types import BufferedInputFile
+# from aiogram.types import FSInputFile
 from decouple import config
 from aiogram import Bot, Dispatcher, types, F
-from database import *
+from database import create_table, connect, close, insert_message, insert_image, get_random_text, get_random_image, \
+    delete_message, delete_image, delete_all_messages, delete_all_images
 from shitpost import shitpost
 from stuff import *
 
 TOKEN = config('BOT_TOKEN')
 NAME = config('DB_NAME')
+OWNER = config('OWNER_ID')
 SIZE = int(config('DEFAULT_SIZE'))
 CHANCE = float(config('DEFAULT_CHANCE'))
 CHANCE_STICKER = float(config('DEFAULT_CHANCE_STICKER'))
@@ -47,15 +50,17 @@ async def post_femcel(message: types.Message, chance: float):
         try:
             await message.answer_sticker(choice(femcel))
         except:
-            await message.asnwer("???")
+            pass
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: types.Message):
-    await message.answer(f"Hi, {message.from_user.full_name}, Im Asa Mitaka. Im autistic and I love shitposting.\n\n"
-                         f"Use /Asa_shitpost to create a shitpost from random last 100 messages and pics,"
-                         f" you can also send me a pic or reply to one with the same command"
-                         f" to create post with specific pic, it works with text too!")
+    await message.answer(f"Hi, {message.from_user.full_name}, Im Asa Mitaka. Im autistic and I love shitposting!\n\n"
+                         f"Use /Asa_shitpost to create a shitpost from last random 100 messages and pics,"
+                         f" you can also send me a pic directly or reply to one with the same command"
+                         f" to create post with the pic you want, it works with text too!\n\n"
+                         f"Use /Asa_delete_message and /Asa_delete_image"
+                         f" to get rid of unwanted pictures and signatures!")
 
 
 @dp.message(Command("Asa_shitpost"))
@@ -82,14 +87,14 @@ async def asa_shitpost(message: types.Message):
             if not text:
                 try:
                     text = message.text[13::]
-                    if not text: text = await get_random_text(message.chat.id)
+                    if not text:
+                        text = await get_random_text(message.chat.id)
                 except:
                     text = await get_random_text(message.chat.id)
         except:
             text = message.text[13::]
             if not text:
                 text = await get_random_text(message.chat.id)
-            await get_random_text(message.chat.id)
     if not text or not img:
         await message.answer("Nah")
         return
@@ -101,11 +106,87 @@ async def asa_shitpost(message: types.Message):
     modified_image_buffer.truncate(0)
 
 
+@dp.message(Command("Asa_delete_message"))
+async def asa_delete_message(message: types.Message):
+    if message.reply_to_message:
+        text = message.text[20::]
+        if not text:
+            text = message.reply_to_message.text
+            if not text:
+                text = message.reply_to_message.caption
+                if not text:
+                    text = None
+    else:
+        try:
+            text = message.caption[20::]
+            if not text:
+                try:
+                    text = message.text[20::]
+                    if not text:
+                        text = None
+                except:
+                    text = None
+        except:
+            text = message.text[20::]
+            if not text:
+                text = None
+    if text:
+        if await delete_message(message.chat.id, text):
+            await message.answer(f'"{text}" deleted, bye bye')
+        else:
+            await message.answer(f'idk what "{text}" is')
+    else:
+        await message.answer("???")
+
+
+@dp.message(Command("Asa_delete_image"))
+async def asa_delete_image(message: types.Message):
+    if message.reply_to_message:
+        try:
+            img = message.reply_to_message.photo[-1].file_id
+        except:
+            img = None
+    else:
+        try:
+            img = message.photo[-1].file_id
+        except:
+            img = None
+    if img:
+        if await delete_image(message.chat.id, img):
+            await message.answer(f"{img} deleted, bye bye")
+        else:
+            await message.answer(f'idk what {img} is')
+    else:
+        await message.answer("???")
+
+
+@dp.message(Command("Asa_delete_all_messages"))
+async def asa_delete_all_messages(message: types.Message):
+    if str(message.from_user.id) == OWNER:
+        await post_femcel(message, 1)
+        await delete_all_messages(message.chat.id)
+        await message.answer("Bam!")
+    else:
+        await message.answer("No way")
+
+
+@dp.message(Command("Asa_delete_all_images"))
+async def asa_delete_all_images(message: types.Message):
+    if str(message.from_user.id) == OWNER:
+        await post_femcel(message, 1)
+        await delete_all_images(message.chat.id)
+        await message.answer("Bam!")
+    else:
+        await message.answer("No way")
+
+
 @dp.message(F.text)
 async def echo_handler(message: types.Message) -> None:
     text = message.text.lower()
     await insert_message(message.chat.id, text)
-    if re.search(r"\b(?:f+e+m+?.?c+e+l+\w*|ф+е+м+?.?ц+е+л+\w*|a+s+a+?.?|а+с+?.?|а+с+а+|m+i+t+a+k+?.?|м+и+т+а+к+?.?)\b", text):
+    if re.search(r"\b(?:f+e+m+?.?c+e+l+\w*|ф+е+м+?.?ц+е+л+\w*|a+s+a+?.?|а+с+?.?|а+с+а+|m+i+t+a+k+?.?|м+и+т+а+к+?.?)\b",
+                 text):
+        await insert_message(message.chat.id, text)
         await post_femcel(message, 1)
         await post_random(message, CHANCE)
         return
@@ -119,7 +200,17 @@ async def echo_handler(message: types.Message) -> None:
 
 @dp.message(F.photo)
 async def echo_photo(message: types.Message) -> None:
+    text = ""
     await insert_image(message.chat.id, message.photo[-1].file_id)
+    try:
+        text = message.text
+    except:
+        pass
+    try:
+        text = message.caption
+    except:
+        pass
+    if text: await insert_message(message.chat.id, text.lower())
     await post_random(message, CHANCE)
     await post_femcel(message, CHANCE_STICKER)
 
@@ -133,6 +224,16 @@ async def echo_sticker(message: types.Message) -> None:
 
 @dp.message(~F.text & ~F.photo & ~F.sticker)
 async def echo_any(message: types.Message):
+    text = ""
+    try:
+        text = message.text
+    except:
+        pass
+    try:
+        text = message.caption
+    except:
+        pass
+    if text: await insert_message(message.chat.id, text.lower())
     await post_random(message, CHANCE)
     await post_femcel(message, CHANCE_STICKER)
 
