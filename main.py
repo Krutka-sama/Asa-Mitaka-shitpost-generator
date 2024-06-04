f833fc01131fd2a8f5be5b4c899458308d9082ae
Krutka-sama <epervyshev@mail.ru>
2024-06-04 07:26:59 +0300

    caption fix

--- main.py
+++ main.py
@@ -231,6 +231,8 @@ async def asa_shitpost(message: types.Message):
             except:
                 try:
                     text = message.reply_to_message.caption
+                    if not text:
+                        text = await get_random_text(message.chat.id)
                 except:
                     text = await get_random_text(message.chat.id)
     else:
